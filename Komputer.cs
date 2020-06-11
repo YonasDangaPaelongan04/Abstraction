6754ccc9eb2e002e32ed057967533e093cf67e5a
yonasdangapaelongan <yonasdanga@gmail.com>
2020-06-12 02:07:55 +0700

    Interface.cs

---
+++ Komputer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    public class Komputer : IKomunikasi
+    {
+        public void Menginput()
+        {
+            Console.WriteLine("Komputer Menginput Dengan Cara Bantuan Hardware lainnya");
+            Console.WriteLine("Dan Dengan Bantuan pengguna nya");
+            Console.WriteLine("Serta Bantuan dari mesin CPU");
+        }
+    }
+}
